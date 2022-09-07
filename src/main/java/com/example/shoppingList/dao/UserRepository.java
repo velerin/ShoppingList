141e7068842ddb2edc2b70611526a038a930d064
velerin <pawel.sobolewski93@gmail.com>
2022-09-07 12:14:55 +0200

    Renaming

--- src/main/java/com/example/shoppingList/dao/UserRepository.java
+++ src/main/java/com/example/shoppingList/dao/UserRepository.java
@@ -1,7 +1,7 @@
 package com.example.shoppingList.dao;
 
-import com.example.shoppingList.entity.User;
+import com.example.shoppingList.entity.UserForList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Integer> {
+public interface UserRepository extends JpaRepository<UserForList,Integer> {
 }
